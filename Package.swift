0416a2bd914c68261a4cafca78b905bf158e31a3
Roman SHamagin <roman.shamagin@7vpn.com>
2023-11-16 15:34:15 +0300

    release

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/daemooon/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
-      checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
+      url: "https://github.com/romaxa55/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
+      checksum: "457f16ce5b68f544959fb54e5490d1347a92232ab7cae0f406f9bc992072e551"
     )
   ]
 )
